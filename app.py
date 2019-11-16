6154fe913865c785b852135dfdfdc5600a29575f
duzqiu <573293679@qq.com>
2019-11-16 23:05:53 +0800

    增加配置 增加邮件发送报告

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import logging
 from logging import handlers
 import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+import configparser
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 APP_KEY = "bcc5494393ba55eec2c107a45d215277"
@@ -29,3 +34,59 @@ def init_logging():
     logger.addHandler(sh)
     logger.addHandler(fh)
     pass
+
+
+class ConfigMessage(object):
+
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.readfp(open('./config/config.ini'))
+
+    def get_config(self, name, key):
+        message = self.config.get(name, key)
+        return message
+
+
+class SendReport(object):
+
+    def __init__(self, filename='report.html'):
+        # 发件人邮箱账号
+        self.sender = ConfigMessage().get_config('email', 'sender')
+        # 发件人邮箱密码
+        self.passwd = ConfigMessage().get_config('email', 'passwd')
+        # 收件人邮箱账号
+        self.receiver = ConfigMessage().get_config('email', 'receiver')
+        # 发送的文件
+        self.filename = filename
+
+    def send_email(self):
+        # print(self.sender, self.passwd, self.receiver, self.filename)
+        # 创建一个带附件的实例
+        message = MIMEMultipart()
+        message['From'] = Header("sender", 'utf-8')
+        message['To'] = Header("receiver", 'utf-8')
+        message['Subject'] = Header('测试报告', 'utf-8')
+
+        # 邮件正文内容
+        message.attach(MIMEText('测试报告请查看附件', 'plain', 'utf-8'))
+
+        # 构造附件1，可构造多个附件
+        report = MIMEText(open(self.filename, 'rb').read(), 'base64', 'utf-8')
+        report["Content-Type"] = 'application/octet-stream'
+        # 这里的filename可以任意写，写什么名字，邮件中显示什么名字
+        report["Content-Disposition"] = 'attachment;filename="report.html"'
+        message.attach(report)
+
+        # 发件人邮箱中的SMTP服务器，端口是465,25
+        server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        # 括号中对应的是发件人邮箱账号、邮箱密码
+        server.login(self.sender, self.passwd)
+        # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.sendmail(self.sender, [self.receiver, ], message.as_string())
+        # 关闭连接
+        server.quit()
+
+
+if __name__ == '__main__':
+    # print(ConfigMessage().get_config("email", "sender"))
+    SendReport("./report/report.html").send_email()
--- run_suite.py
+++ run_suite.py
@@ -4,6 +4,7 @@ from script.api_ip_location import IpQueryTest
 from script.api_moblie_home import MoblieHomeTest
 from script.api_weather_query import WeatherQueryTest
 from script.api_new_handline import NewHandlineTest
+from app import SendReport
 import time
 
 # 生成测试套件
@@ -16,8 +17,11 @@ suite.addTest(unittest.makeSuite(NewHandlineTest))
 # 定义报告名称
 # report_path = "./report/api-{}.html".format(time.strftime("%Y%m%d %H%M%S"))
 report_path = "./report/report.html"
-print(report_path)
+# print(report_path)
 # 使用HTMLTestRunner生成测试报告
 with open(report_path, 'wb') as f:
     runner = HTMLTestRunner(f, verbosity=1, title="接口自动化测试", description="V1.0")
     runner.run(suite)
+
+# 调用邮件发送
+SendReport(report_path).send_email()
